911b69f2fc188a6fdfb808dcc067a05978d6ed3e
gcygan2 <prezentacje@gcygan.webd.pl>
2026-09-24 21:53:03 +0200

    Update kwiatki3.ino

--- kwiatki3.ino
+++ kwiatki3.ino
@@ -1,11 +1,10 @@
 #include <WiFi.h>
-#include <WiFiMulti.h>
+
 #define PRZYCISK 2
 void setup() {
   pinMode (PRZYCISK, INPUT_PULLUP);
   WiFi.mode(WIFI_STA);
   Serial.begin(115200);
-
 
   WiFi.begin("ssid", "haslo");
   while (WiFi.status() != WL_CONNECTED) {
